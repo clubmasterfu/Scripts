bf5c55cfb47f0d3271178f85311d5591eb7b68cf
clubmasterfu <61084108+clubmasterfu@users.noreply.github.com>
2020-09-18 23:04:20 +0200

    Powershell Mouse Simulator

---
+++ MouseWiggle.ps1
@@ -0,0 +1,10 @@
+﻿#RemoteSigned --> lokale Script ausführe
+
+function MouseWiggle {
+    Add-Type -Assembly System.Windows.Forms
+    while($true) {
+        Start-Sleep -Seconds 1
+        [Windows.Forms.Cursor]::Position = New-Object Drawing.Point(random 1000),(random 1000)
+    }
+}
+MouseWiggle
